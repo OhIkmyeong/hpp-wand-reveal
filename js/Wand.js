28ad4349a862692e6f16e5c239dbb06cf246ba0c
Oh JuYeon <dpffpself@naver.com>
2023-07-12 10:15:11 +0900

    2023-07-12

--- js/Wand.js
+++ js/Wand.js
@@ -1,29 +1,29 @@
-export class Wand{
-    constructor(){
+export class Wand {
+    constructor() {
         this.$wand = document.getElementById('wand');
         this.get_width();
     }//constructor
 
-    get_width(){
+    get_width() {
         this.winWid = window.innerWidth;
         this.winHei = window.innerHeight;
         this.winWidHalf = window.innerWidth / 2;
         this.width = this.$wand.getBoundingClientRect().width;
     }//get_width
 
-    move_wand = (e)=>{
-        const {clientX,clientY} = e;
+    move_wand = (e) => {
+        const { clientX, clientY } = e;
         // this.$wand.style.transform = `translate(${clientX - (this.width / 2)}px, ${clientY}px)`;
         const per = clientX / this.winWid;
         const deg = (per * 50) - 25; //-10 ~ 10이라 20 곱하고 -10 뺀거임
-        const wandX =(this.winWid * -0.15) + clientX * 1.3;
+        const wandX = (this.winWid * -0.15) + clientX * 1.3;
         console.log(wandX);
         const wandY = (this.winHei * 0.1) + clientY * 0.4;
         this.$wand.animate([
-            {transform : `translate(${wandX}px, ${wandY}px) rotate(${deg}deg)`}
-        ],{
-            duration : 500,
-            fill : "forwards" 
+            { transform: `translate(${wandX}px, ${wandY}px) rotate(${deg}deg)` }
+        ], {
+            duration: 500,
+            fill: "forwards"
         });
     }//move_wand
 }//class-Wand
